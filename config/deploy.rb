9d9250ecd7fb8d6f4ed8a7afea46a6a1d524bb1d
Corey Collins <coreyecollins@gmail.com>
2013-12-18 08:35:37 -0600

    add zero run time

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,23 @@ set :keep_releases, 5
 
 set :rvm_ruby_version, '1.9.3-p484'
 set :default_env, { path: "~/.rvm/bin:$PATH" }
-#SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
+#git commit -amSSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
 
-#after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
+namespace :deploy do
+
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "cd #{current_path}; #{sudo} kill -s USR2 `cat tmp/pids/unicorn.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; sudo bundle exec unicorn_rails -E production -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "sudo kill -s QUIT `cat #{File.join(current_path,'tmp/pids/unicorn.pid')}`"
+  end  
+  
+end
